bfee5e3df30481fbb394bf12bc7c85e31994e960
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-08-28 18:49:02 +0300

    refactor: rename renderElement to render

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import TaskEdit from './view/task-edit.js';
 import LoadMoreButton from './view/load-more-button.js';
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
-import {renderElement, RenderPosition} from "./utils.js";
+import {render, RenderPosition} from "./utils.js";
 
 const TASK_COUNT = 22;
 const TASK_COUNT_PER_STEP = 8;
@@ -19,23 +19,23 @@ const filters = generateFilter(tasks);
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-renderElement(siteHeaderElement, new SiteMenu().getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new Filter(filters).getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, new SiteMenu().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new Filter(filters).getElement(), RenderPosition.BEFOREEND);
 
 const boardComponent = new Board();
 
-renderElement(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
-renderElement(boardComponent.getElement(), new Sort().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), new Sort().getElement(), RenderPosition.BEFOREEND);
 
 const tasksListComponent = new Tasks();
 
-renderElement(boardComponent.getElement(), tasksListComponent.getElement(), RenderPosition.BEFOREEND);
+render(boardComponent.getElement(), tasksListComponent.getElement(), RenderPosition.BEFOREEND);
 
 
-renderElement(tasksListComponent.getElement(), new TaskEdit(tasks[0]).getElement(), RenderPosition.BEFOREEND);
+render(tasksListComponent.getElement(), new TaskEdit(tasks[0]).getElement(), RenderPosition.BEFOREEND);
 
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  renderElement(tasksListComponent.getElement(), new Task(tasks[i]).getElement(), RenderPosition.BEFOREEND);
+  render(tasksListComponent.getElement(), new Task(tasks[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
 if (tasks.length > TASK_COUNT_PER_STEP) {
@@ -43,13 +43,13 @@ if (tasks.length > TASK_COUNT_PER_STEP) {
 
   const loadMoreButtonComponent = new LoadMoreButton();
 
-  renderElement(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
   loadMoreButtonComponent.getElement().addEventListener(`click`, (evt) => {
     evt.preventDefault();
     tasks
       .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-      .forEach((task) => renderElement(tasksListComponent.getElement(), new Task(task).getElement(), RenderPosition.BEFOREEND));
+      .forEach((task) => render(tasksListComponent.getElement(), new Task(task).getElement(), RenderPosition.BEFOREEND));
 
     renderedTaskCount += TASK_COUNT_PER_STEP;
 
