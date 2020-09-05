622e709221624dc1e319825e925a77d361694c4f
Yulia <juliam2007@mail.ru>
2020-09-05 22:09:04 +0300

    refactor: the logic of the loadMoreButton

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -2,25 +2,30 @@ import Board from "../view/board.js";
 import Sort from "../view/sort.js";
 import Tasks from "../view/tasks.js";
 import NoTask from "../view/no-task.js";
-import {remove, render, RenderPosition, replace} from "../utils/render";
-import Task from "../view/task";
-import TaskEdit from "../view/task-edit";
-import LoadMoreButton from "../view/load-more-button";
+import Task from "../view/task.js";
+import TaskEdit from "../view/task-edit.js";
+import LoadMoreButton from "../view/load-more-button.js";
+import {render, RenderPosition, replace, remove} from "../utils/render.js";
 
 const TASK_COUNT_PER_STEP = 8;
 
 export default class BoardPresenter {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
+    this._renderedTaskCount = TASK_COUNT_PER_STEP;
 
     this._boardComponent = new Board();
     this._sortComponent = new Sort();
     this._taskListComponent = new Tasks();
     this._noTaskComponent = new NoTask();
+    this._loadMoreButtonComponent = new LoadMoreButton();
+
+    this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
   }
 
   init(boardTasks) {
     this._boardTasks = boardTasks.slice();
+
     render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
 
@@ -32,8 +37,6 @@ export default class BoardPresenter {
   }
 
   _renderTask(task) {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTask в main.js
     const taskComponent = new Task(task);
     const taskEditComponent = new TaskEdit(task);
 
@@ -76,24 +79,19 @@ export default class BoardPresenter {
     render(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
 
+  _handleLoadMoreButtonClick() {
+    this._renderTasks(this._renderedTaskCount, this._renderedTaskCount + TASK_COUNT_PER_STEP);
+    this._renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (this._renderedTaskCount >= this._boardTasks.length) {
+      remove(this._loadMoreButtonComponent);
+    }
+  }
+
   _renderLoadMoreButton() {
-    let renderedTaskCount = TASK_COUNT_PER_STEP;
+    render(this._boardComponent, this._loadMoreButtonComponent, RenderPosition.BEFOREEND);
 
-    const loadMoreButtonComponent = new LoadMoreButton();
-
-    render(this._boardComponent, loadMoreButtonComponent, RenderPosition.BEFOREEND);
-
-    loadMoreButtonComponent.setClickHandler(() => {
-      this._boardTasks
-        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-        .forEach((boardTask) => this._renderTask(boardTask));
-
-      renderedTaskCount += TASK_COUNT_PER_STEP;
-
-      if (renderedTaskCount >= this._boardTasks.length) {
-        remove(loadMoreButtonComponent);
-      }
-    });
+    this._loadMoreButtonComponent.setClickHandler(this._handleLoadMoreButtonClick);
   }
 
   _renderTaskList() {
@@ -111,8 +109,6 @@ export default class BoardPresenter {
     }
 
     this._renderSort();
-
     this._renderTaskList();
   }
-
 }
