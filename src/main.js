0fea3177d9e8021094be5987b5a17732f9c588a8
Yulia <juliam2007@mail.ru>
2020-11-30 16:47:59 +0300

    feature: add class api

--- src/main.js
+++ src/main.js
@@ -8,10 +8,22 @@ import FilterModel from "./model/filter";
 import {MenuItem, UpdateType, FilterType} from "./const.js";
 import Statistics from "./view/statistics";
 import {remove} from "./utils/render";
+import Api from "./api.js";
 
 const TASK_COUNT = 22;
+const AUTHORIZATION = `Basic hS2sd3dfSwcl1sa2j`;
+const END_POINT = `https://12.ecmascript.pages.academy/task-manager`;
 
 const tasks = new Array(TASK_COUNT).fill(``).map(generateTask);
+const api = new Api(END_POINT, AUTHORIZATION);
+
+api.getTasks().then((tasks) => {
+  console.log(tasks);
+  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+  // а ещё на сервере используется snake_case, а у нас camelCase.
+  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+  // Есть вариант получше - паттерн "Адаптер"
+});
 
 let statisticsComponent = null;
 
